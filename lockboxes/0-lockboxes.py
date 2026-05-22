36a2fb581432b5cbf23f3b9e90a3f4d34ac6b39d
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-22 14:14:51 +0200

    add dir

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Lockboxes problem module
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Determine if all boxes can be opened.
+
+    Args:
+        boxes (list of list of int): List where each index represents a box
+        and contains keys to other boxes.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
+    if not boxes:
+        return True
+
+    n = len(boxes)
+    opened = set([0])  # we start with box 0 unlocked
+    keys = set(boxes[0])
+
+    while keys:
+        key = keys.pop()
+
+        if 0 <= key < n and key not in opened:
+            opened.add(key)
+            keys.update(boxes[key])
+
+    return len(opened) == n
