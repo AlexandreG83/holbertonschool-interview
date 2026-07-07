af07cfef76bee8ea94b0c366f992be172f6b0eb9
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-07-07 16:00:59 +0200

    island perimeter project

---
+++ island_perimeter/0-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module that computes the perimeter of an island.
+"""
+
+def island_perimeter(grid):
+    """
+    Return the perimeter of the island described in grid.
+    """
+    perimeter = 0
+    len_grid = len(grid)
+    for row in range(len_grid):
+        for col in range(len_grid):
+            if grid[row][col] == 1:
+                perimeter += 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+    return perimeter
